2bfeb75330738ddccdc82ab50003041f4cc93434
Alireza afshar <35702359+alire7a@users.noreply.github.com>
2023-04-21 04:42:30 +0400

    footer 3styles done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ContactUsC from "./components/contact-us/ContactUsC";
 import ContactUsB from "./components/contact-us/ContactUsB";
 import ContactUsBoheimian from "./components/contact-us/ContactUsBoheimian";
 import HeroCardC from "./components/hero-card/HeroCardC";
+import FooterS1C from "./components/Footer/Classic/Footer-S1-C"
+import FooterS2C from "./components/Footer/Classic/Footer-S2-C";
+import FooterS3C from "./components/Footer/Classic/Footer-S3-C";
+import FooterS1M from "./components/Footer/Modern/Footer-S1-M";
+import FooterS2M from "./components/Footer/Modern/Footer-S2-M";
+import FooterS3M from "./components/Footer/Modern/Footer-S3-M";
+import FooterS1B from "./components/Footer/Boheimian/Footer-S1-B";
+import FooterS2B from "./components/Footer/Boheimian/Footer-S2-B";
+import FooterS3B from "./components/Footer/Boheimian/Footer-S3-B";
 function App() {
   return (
     <>
@@ -14,6 +23,23 @@ function App() {
             <Route path="contact-us-b" element={<ContactUsB />} />
             <Route path="contact-us-boheimian" element={<ContactUsBoheimian />} />
             <Route path="hero-card-c" element={<HeroCardC />} />
+
+            <Route path="footer" element={
+              <div className={"page bg-red"}>
+                <FooterS1B/>
+                <FooterS2B/>
+                <FooterS3B/>
+
+                <FooterS1M/>
+                <FooterS2M/>
+                <FooterS3M/>
+
+                <FooterS1C/>
+                <FooterS2C/>
+
+                <FooterS3C/>
+              </div>
+            } />
           </Route>
         </Routes>
       </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,67 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/**/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      darkBlue: "#374957",
-      blackBg: "#2C2C2C",
-      white: "#FFFFFF",
+    maxWidth:{
+      "max":"1500px",
     },
+    colors: {
+      "white":"#fff",
+      "primary":"#373131",
+      'black': '#0D0807',
+      "green": '#2BAC76',
+      'red': '#CD2553',
+      "purple":"#9DA6FA",
+      "gray":{
+        "light":"#F8F9FF",
+      },
+      "transparent":"transparent"
+    },
+    borderWidth:{
+      "1":"1px",
+      "2":"2px",
+    },
+
+    fontSize: {
+      '12': '12px',
+      '14': '14px',
+      '16': '16px',
+      '18': '18px',
+      '20': '20px',
+      '24': '24px',
+      '28': '1.875rem',
+      '32': '2.25rem',
+      '36': '36px',
+      '40': '40px',
+      '48': '48px',
+      '56': ["56px","64px"],
+      '64': '64px',
+      '120': '120px',
+    },
+    borderRadius: {
+      'none': '0',
+      "8" : "8px",
+      "12" : "12px",
+      "16" : "16px",
+      "full":"100%",
+    },
+    screens:{
+      'sm': '640px',
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+      "fhd":"1920px"
+    }
+    ,
+
+
+    extend: {},
   },
   plugins: [],
 }
